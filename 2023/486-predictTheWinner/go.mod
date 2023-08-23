25f00b1120782d67a7f4667f22f479a993ddc7cb
Jesus Marron <jesusmarron7@gmail.com>
2023-08-22 19:19:43 -0500

    finished predictTheWinner

---
+++ 2023/486-predictTheWinner/go.mod
@@ -0,0 +1,3 @@
+module github.com/jmarron7/leetcode-exercises/486-predictTheWinner
+
+go 1.18
---
+++ 2023/486-predictTheWinner/main.go
@@ -0,0 +1,37 @@
+func predictTheWinner(nums []int) bool {
+    N := len(nums)
+
+	hasCache := make([][]bool, N+1)
+	cache := make([][]int, N+1)
+	for i := range hasCache {
+		hasCache[i] = make([]bool, N+1)
+		cache[i] = make([]int, N+1)
+	}
+
+	var score func(int, int) int
+	score = func(left, right int) int {
+		if left > right {
+			return 0
+		}
+
+		if hasCache[left][right] {
+			return cache[left][right]
+		}
+
+		scoreTakingLeft := nums[left] - score(left+1, right)
+		scoreTakingRight := nums[right] - score(left, right-1)
+
+		hasCache[left][right] = true
+		cache[left][right] = max(scoreTakingLeft, scoreTakingRight)
+		return cache[left][right]
+	}
+
+	return score(0, N-1) >= 0
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
